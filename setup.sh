2c04c913d7833e100883659d4a91497f73ced819
Samuel Burger <xburger2@fi.muni.cz>
2024-10-04 19:01:02 +0200

    working setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo $(pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+if [ -d ~/.config/nvim ]; then
+    rm -rf ~/.config/nvim
+fi
+
+ln -s "${SCRIPT_DIR}/nvim" "${HOME}/.config/nvim"
+
+for file in .*; do
+    if [ "${file}" = ".." ] || [ "${file}" = "." ] || [ "${file}" = ".git" ] || [ "${file}" = ".gitignore" ]; then
+        continue
+    fi
+
+    echo "Seting: ${HOME}/${file}"
+
+    rm -f "${HOME}/${file}"
+
+    ln -s "${SCRIPT_DIR}/${file}" "${HOME}/${file}"
+done
+
+echo "DONE"
